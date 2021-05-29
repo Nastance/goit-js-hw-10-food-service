065a1b68660cadbcd420cd998126d4edacf881a0
Anastasiia Haltseva <galtseva.wait@gmail.com>
2021-05-29 23:39:59 +0300

    added menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './css/styles.css';
 import cards from './menu.json';
+import itemsTempl from './templates/template.hbs';
+
+const menuRef = document.querySelector('.js-menu');
+const menuList = itemsTempl(cards);
+menuRef.insertAdjacentHTML('beforeend', menuList);
