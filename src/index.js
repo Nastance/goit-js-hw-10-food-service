a373ef476c419e18dfc127f57ab6a84c668359a7
Anastasiia Haltseva <galtseva.wait@gmail.com>
2021-05-30 13:21:20 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cards from './menu.json';
 import itemsTempl from './templates/template.hbs';
 
 const menuRef = document.querySelector('.js-menu');
-console.log(menuRef);
+// console.log(menuRef);
 const menuList = itemsTempl(cards);
 menuRef.insertAdjacentHTML('beforeend', menuList);
 
@@ -13,28 +13,29 @@ const Theme = {
 };
 
 const bodyRef = document.querySelector('body');
-console.log(bodyRef);
+// console.log(bodyRef);
 
-const themeChangerRef = document.querySelector('#theme-switch__toggle');
-console.log(themeChangerRef);
+const themeChangerRef = document.querySelector('#theme-switch-toggle');
+// console.log(themeChangerRef);
 themeChangerRef.addEventListener('change', changeThemeHandler);
 
 function changeThemeHandler(event) {
-    document.body.classList.toggle(Theme.LIGHT);
-    document.body.classList.toggle(Theme.DARK);
+    bodyRef.classList.toggle(Theme.LIGHT);
+    bodyRef.classList.toggle(Theme.DARK);
 
-    if (event.target.checked) {
-        localStorage.setItem('theme', Theme.DARK)
-    } else {
-        localStorage.setItem('theme', Theme.LIGHT)
-    }
+    localStorage.setItem('theme', bodyRef.className);
+    // if (event.target.checked) {
+    //     localStorage.setItem('theme', Theme.DARK)
+    // } else {
+    //     localStorage.setItem('theme', Theme.LIGHT)
+    // }
 };
 
 function chackThemeHandler() {
-    const sevedTheme = localStorage.getItem('theme');
-    console.log(savedTheme);
+    const savedTheme = localStorage.getItem('theme');
+    // console.log(savedTheme);
 
-    if (sevedTheme === Theme.DARK) {
+    if (savedTheme === Theme.DARK) {
         bodyRef.classList.add(Theme.DARK);
         themeChangerRef.chacked = true;
     } else {
