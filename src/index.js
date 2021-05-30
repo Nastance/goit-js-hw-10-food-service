6aac44ae95ec51a4e0527d9dbc799d8736dac7e3
Anastasiia Haltseva <galtseva.wait@gmail.com>
2021-05-30 11:51:38 +0300

    aded theme Changer

--- src/index.js
+++ src/index.js
@@ -3,5 +3,43 @@ import cards from './menu.json';
 import itemsTempl from './templates/template.hbs';
 
 const menuRef = document.querySelector('.js-menu');
+console.log(menuRef);
 const menuList = itemsTempl(cards);
 menuRef.insertAdjacentHTML('beforeend', menuList);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+console.log(bodyRef);
+
+const themeChangerRef = document.querySelector('#theme-switch__toggle');
+console.log(themeChangerRef);
+themeChangerRef.addEventListener('change', changeThemeHandler);
+
+function changeThemeHandler(event) {
+    document.body.classList.toggle(Theme.LIGHT);
+    document.body.classList.toggle(Theme.DARK);
+
+    if (event.target.checked) {
+        localStorage.setItem('theme', Theme.DARK)
+    } else {
+        localStorage.setItem('theme', Theme.LIGHT)
+    }
+};
+
+function chackThemeHandler() {
+    const sevedTheme = localStorage.getItem('theme');
+    console.log(savedTheme);
+
+    if (sevedTheme === Theme.DARK) {
+        bodyRef.classList.add(Theme.DARK);
+        themeChangerRef.chacked = true;
+    } else {
+        bodyRef.classList.add(Theme.LIGHT);
+    }
+}
+
+chackThemeHandler();
